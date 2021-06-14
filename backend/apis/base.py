4affd5359dc73ebe931155eb5cb5c7998d0179cb
Dennis Berg <dennis.berg5@gmail.com>
2021-06-13 22:20:58 -0700

    add user route

---
+++ backend/apis/base.py
@@ -0,0 +1,9 @@
+# jobboard/backend/apis/version1/base.py
+
+from fastapi import APIRouter
+from apis.version1 import route_users
+
+
+api_router = APIRouter()
+
+api_router.include_router(route_users.router,prefix="/user",tags=["users"])
--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -9,3 +9,11 @@ class UserCreate(BaseModel):
     email : EmailStr
     password : str
     
+
+class ShowUser(BaseModel):
+    username : str
+    email : EmailStr
+    is_active : bool
+
+    class Config():
+        orm_mode = True
